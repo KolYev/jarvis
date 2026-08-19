f2ec92842e93b0ef696f4cd33521d17e6da595be
KolYev <evgeniy.kolesenko@gmail.com>
2026-08-19 21:11:08 +0300

    Функция для поиска в DuckDuckGo

--- jarvis.py
+++ jarvis.py
@@ -1,6 +1,7 @@
 from openai import OpenAI
 import os
 from dotenv import load_dotenv
+from duckduckgo_search import DDGS
 
 load_dotenv()
 
@@ -11,13 +12,26 @@ client = OpenAI(
     api_key=API_KEY
 )
 
+# поиск информации в DuckDuckGo
+def websearch(query, max_results=3):
+    with DDGS() as ddgs:
+        results = list(ddgs.text(query, max_results=max_results))
+    if not results:
+        return "Ничего не найдено."
+    snippets = []
+    for r in results:
+        snippets.append(f"Заголовок: {r['title']}\nСсылка: {r['href']}\nТекст: {r['body']}")
+    return "\n\n".join(snippets)
+
 print("Вы: ", end="")
 message = str(input())
+
+prompt = ("Твоё имя - Джарвис. Используй информацию из своей памяти и из результатов поиска для ответов на вопросы.")
 
 responce = client.chat.completions.create(
     model="local-model",
     messages=[
-        {"role": "system", "content": "Твоё имя - Джарвис"},
+        {"role": "system", "content": prompt},
         {"role": "user", "content": message}
     ],
     temperature=0.7
