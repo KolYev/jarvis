596b4b4d9f11e3a15f519655e92a2e7bdefad476
KolYev <evgeniy.kolesenko@gmail.com>
2026-08-19 19:53:50 +0300

    Получение первого сообщения от нейронки

--- jarvis.py
+++ jarvis.py
@@ -10,3 +10,14 @@ client = OpenAI(
     base_url="http://localhost:1234/v1",
     api_key=API_KEY
 )
+
+responce = client.chat.completions.create(
+    model="local-model",
+    messages=[
+        {"role": "system", "content": "Твоё имя - Джарвис"},
+        {"role": "user", "content": "Привет! Как тебя зовут?"}
+    ],
+    temperature=0.7
+)
+
+print(responce.choices[0].message.content)
