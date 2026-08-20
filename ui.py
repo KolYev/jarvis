d8fa06130e9930f741ee821eeb890283677fa711
KolYev <evgeniy.kolesenko@gmail.com>
2026-08-20 17:41:15 +0300

    ui.py: добавляем текст и иконку микрофона в интерфейс

--- ui.py
+++ ui.py
@@ -2,5 +2,17 @@ import flet as ft
 
 def main(page: ft.Page):
     page.title = "Джарвис"
+    page.theme_mode = 'dark'
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+
+    page.add(
+        ft.Row(
+            [ 
+                ft.Text('Джарвис'),
+                ft.Icon(icon=ft.Icons.MIC)
+            ],
+            alignment=ft.MainAxisAlignment.CENTER
+        )
+    )
 
 ft.app(target=main)
