614cf5aff8c48456c10dc4de07e6c4fd793d7f66
KolYev <evgeniy.kolesenko@gmail.com>
2026-08-22 13:09:33 +0300

    jarvis_memory.py: Функция для сохранения файла

--- jarvis_memory.py
+++ jarvis_memory.py
@@ -9,3 +9,8 @@ def _load():
         return {"profile": {}, "facts": []}
     with open(MEMORY_FILE, "r", encoding="utf-8") as f:
         return json.load(f)
+
+def _save(data):
+    with open(MEMORY_FILE, "w", encoding="utf-8") as f:
+        json.dump(data, f, ensure_ascii=False, indent=2)
+
