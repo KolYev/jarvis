f19171d6ea6941411a29ef6e6474f879f91d5b51
KolYev <evgeniy.kolesenko@gmail.com>
2026-08-22 18:31:05 +0300

    ui.py: ширина и высота экрана с нулевыми отступами

--- ui.py
+++ ui.py
@@ -3,7 +3,10 @@ import flet as ft
 def main(page: ft.Page):
     page.title = "Джарвис"
     page.theme_mode = 'dark'
-    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    page.window_width = 480
+    page.window_height = 760
+    page.padding = 0
+    page.vertical_alignment = ft.MainAxisAlignment.START
 
     page.add(
         ft.Row(
